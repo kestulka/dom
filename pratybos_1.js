025fe4977fe92286ceb0d978ab62589bd776696c
kestulka <kgirdiunas0@gmail.com>
2023-12-13 22:30:56 +0200

    .

---
+++ pratybos_1.js
@@ -0,0 +1,155 @@
+// 1. Sukurti elementa ir i ji ideti savo sugalvota teksta
+
+// function addElement() {
+//   const newDiv = document.createElement("div");
+//   const newContent = document.createTextNode("mano sugalvoltas tekstas");
+//   newDiv.appendChild(newContent);
+//   const currentDiv = document.getElementById("div1");
+//   document.body.insertBefore(newDiv, currentDiv);
+// }
+// addElement();
+
+// 2. Sukurti elementa ir i ji patalpinti kita elementa su tekstu
+
+// nr.1
+// const divas = document.createElement("div");
+// console.log(divas);
+// const pTekstas = document.createElement("p");
+// console.log(pTekstas);
+// divas.appendChild(pTekstas);
+
+// nr.2
+// const divas = document.querySelector("div");
+// console.log(divas);
+// const textDiv = document.createElement("p");
+// divas.appendChild(textDiv);
+// textDiv.innerText = "skibidi pop pop yes yes";
+
+// 3. Pasizymeti elementa ir prideti jam atributa su reiksme: a) klase "myClass"; b) id "myId"; c) <a> ir <img> tagu atributus su reiksmemis
+
+// basic budas:
+
+// const anchoras = document.getElementById("anchor1");
+// anchoras.setAttribute("class", "myClass");
+// anchoras.setAttribute("id", "myId");
+// anchoras.setAttribute("href", "https://www.google.com");
+// anchoras.setAttribute("src", "cat.jpeg");
+// console.log(anchoras);
+
+// budas su funkcija:
+
+// const anchoras = document.getElementById("anchor1");
+
+// function setAttributes(element, attributes) {
+//   for (let key in attributes) {
+//     element.setAttribute(key, attributes[key]);
+//   }
+// }
+
+// setAttributes(anchoras, {
+//   class: "myClass",
+//   id: "myId",
+//   href: "https://www.google.com",
+//   src: "cat.jpeg",
+// });
+// console.log(anchoras);
+
+// 4. sukurti keleta "p" tagu div viduje, pasizymeti elementu masyva ir prideti jiems stiliu - kas antro elemento raudonas tekstas ir juodas background, likusiu tekstas baltas, background zalias
+// const spalvotasDivas = document.querySelectorAll("p");
+// console.log(spalvotasDivas);
+// spalvotasDivas.forEach(function (element, index) {
+//   if (index % 2 === 1) {
+//     element.style.color = "red";
+//     element.style.backgroundColor = "black";
+//   } else {
+//     element.style.color = "white";
+//     element.style.backgroundColor = "green";
+//   }
+// });
+// console.log(spalvotasDivas);
+
+// const spalvotasDivas = document.getElementById("spalvuDiv");
+// const list = spalvotasDivas.querySelectorAll("p");
+// console.log(list);
+
+// 5. Paimti reiksme is input ir ja atspausdinti konsoleje bei atvaizduoti puslapyje
+
+// const forma = document.querySelector("form");
+// const inputas = document.querySelector("input");
+// const submitas = document.querySelector("form > button");
+
+// const outputas = document.createElement("span");
+// document.body.appendChild(outputas);
+// // console.log(outputas);
+
+// forma.addEventListener("submit", (event) => {
+//   event.preventDefault();
+//   const inputValue = inputas.value;
+//   outputas.textContent = inputValue;
+//   console.log(inputValue);
+// });
+
+// 6. Sukurti du mygtukus: paspaudus pirmaji, sukuriamas elementas, paspaudus antraji - elementas pasalinimas. Mygtukus stilizuoti su JS.
+// 7. sukurti click counter - kad kiekvienas mygtuko paspaudimas butu registruojas. Skaicius turi matytis salia mygtuko
+
+const button1 = document.createElement("button");
+button1.innerText = "create element";
+const button2 = document.createElement("button");
+button2.innerText = "delete element";
+
+// kontikas button'am kad matytusi puslapyje
+const divOfButtons = document.getElementById("divOfButtons");
+divOfButtons.append(button1, button2);
+
+// kintamieji
+let elementas;
+let button1DefaultColor = "white";
+let button2DefaultColor = "white";
+let skaitliukasCreate = 0;
+let skaitliukasDelete = 0;
+
+const counter1 = document.createElement("text");
+counter1.innerText = ` (times clicked: ${skaitliukasCreate})`;
+button1.appendChild(counter1);
+
+const counter2 = document.createElement("text");
+counter2.innerText = ` (times clicked: ${skaitliukasDelete})`;
+button2.appendChild(counter2);
+
+// klausytojas + skaitliukas
+button1.addEventListener("click", (event) => {
+  event.preventDefault();
+  skaitliukasCreate++;
+
+  elementas = document.createElement("p");
+  elementas.innerText = "created new element";
+  console.log("created new element");
+
+  button1.style.backgroundColor = "green";
+  setTimeout(() => {
+    button1.style.backgroundColor = button2DefaultColor;
+  }, 100);
+
+  // updatintas skaitliukas (BUTINA)
+  counter1.innerText = ` (times clicked: ${skaitliukasCreate})`;
+});
+
+button2.addEventListener("click", (event) => {
+  event.preventDefault();
+  skaitliukasDelete++;
+
+  // tikrina ar egzistuoja elementas, jei egzistuoja istrina ir atspausdina
+  if (elementas) {
+    elementas.remove();
+    console.log("element has been deleted");
+    button2.style.backgroundColor = "red";
+    setTimeout(() => {
+      button2.style.backgroundColor = button2DefaultColor;
+    }, 100);
+  } else {
+    console.log("you did not delete any elements. (no elements left)");
+  }
+
+  // updatintas skaitliukas (BUTINA)
+  counter2.innerText = ` (times clicked: ${skaitliukasDelete})`;
+});
--- script.js
+++ script.js
@@ -1,111 +1,165 @@
-const divas1=document.querySelector("div")
-const divas2=document.getElementById("divSuId")
-const divas3=document.getElementsByClassName("divSuClass")
-const visi=document.querySelectorAll("div")
-console.log(divas1)
-console.log(divas2)
-console.log(divas3)
-console.log(visi)
+// const divas1 = document.querySelector("div");
+// const divas2 = document.getElementById("divSuId");
+// const divas3 = document.getElementsByClassName("divSuClass");
+// const visi = document.querySelectorAll("div");
+// console.log(divas1);
+// console.log(divas2);
+// console.log(divas3);
+// console.log(visi);
 
-const child1=divas2.querySelector("p")
-console.log(child1)
-const child2=document.querySelector("div").childNodes;
-console.log(child2)
-const child3=document.querySelectorAll("div > p")
-console.log(child3)
-const firstChild = document.querySelector("divSuId > p")
-console.log(firstChild, child3[0])
-child3[0].setAttribute("mano", "kaciukas")
-child3[1].setAttribute("class", "kaciukas")
-child3[0].removeAttribute("mano")
-const attr = child3[1].getAttribute("class")
-console.log(attr)
+// const child1 = divas2.querySelector("p");
+// console.log(child1);
+// const child2 = document.querySelector("div").childNodes;
+// console.log(child2);
+// const child3 = document.querySelectorAll("div > p");
+// console.log(child3);
+// const firstChild = document.querySelector("divSuId > p");
+// console.log(firstChild, child3[0]);
+// child3[0].setAttribute("mano", "kaciukas");
+// child3[1].setAttribute("class", "kaciukas");
+// child3[0].removeAttribute("mano");
+// const attr = child3[1].getAttribute("class");
+// console.log(attr);
 
-child3[0].classList.add("myClassInCss")
-child3[0].classList.remove("myClassInCss")
-child3[0].classList.replace("antrasP", "myClassInCss")
-child3[0].classList.toggle("myClassInCss")
-child3[1].style.backgroundColor='purple'
+// child3[0].classList.add("myClassInCss");
+// child3[0].classList.remove("myClassInCss");
+// child3[0].classList.replace("antrasP", "myClassInCss");
+// child3[0].classList.toggle("myClassInCss");
+// child3[1].style.backgroundColor = "purple";
 
-const myButton = document.createElement("button")
-child3[0].appendChild(myButton)
+// const myButton = document.createElement("button");
+// child3[0].appendChild(myButton);
 
-divas1.innerHTML="<span> as nesaugus </span>"
+// divas1.innerHTML = "<span> as nesaugus </span>";
 
-myButton.innerText="klik me"
+// myButton.innerText = "klik me";
 
-const span = document.querySelector("span")
-// span.remove()
+// const span = document.querySelector("span");
+// // span.remove()
 
-myButton.addEventListener("click", ()=>{
-    if(myButton.style.backgroundColor='pink'){
-        myButton.style.backgroundColor='blue'
-    }else{
-        myButton.style.backgroundColor='pink'
-    }
-})
+// myButton.addEventListener("click", () => {
+//   if ((myButton.style.backgroundColor = "pink")) {
+//     myButton.style.backgroundColor = "blue";
+//   } else {
+//     myButton.style.backgroundColor = "pink";
+//   }
+// });
 
-const buttons = document.getElementsByClassName("buttonClass");
-for (const button of buttons) {
-    button.addEventListener('click', () => console.log("clicked!"))
-}
+// const buttons = document.getElementsByClassName("buttonClass");
+// for (const button of buttons) {
+//   button.addEventListener("click", () => console.log("clicked!"));
+// }
 
-const myButton2 = document.createElement("button")
-myButton2.innerText='miau'
-const myButton3 = document.createElement("button")
-myButton3.innerText='miau'
+// const myButton2 = document.createElement("button");
+// myButton2.innerText = "miau";
+// const myButton3 = document.createElement("button");
+// myButton3.innerText = "miau";
 
-divas2.append(myButton2, myButton3)
+// divas2.append(myButton2, myButton3);
 
-const buttonsGroup = document.querySelectorAll("button")
-console.log(buttonsGroup)
+// const buttonsGroup = document.querySelectorAll("button");
+// console.log(buttonsGroup);
 
-buttonsGroup.forEach((el, index)=> el.addEventListener("click", ()=> {
-    console.log(`you clicked button "${el.innerText}"`)
+// buttonsGroup.forEach((el, index) =>
+//   el.addEventListener("click", () => {
+//     console.log(`you clicked button "${el.innerText}"`);
+//   }),
+// );
 
-}))
+// const forma = document.querySelector("form");
+// const inputas = document.querySelector("input");
+// const submitas = document.querySelector("form > button");
+// console.log(forma, inputas, submitas);
 
-const forma = document.querySelector("form")
-const inputas = document.querySelector("input")
-const submitas = document.querySelector("form > button")
-console.log(forma, inputas, submitas)
+// const arrej = [];
+// let counter = 0;
 
-const arrej = []
-    let counter = 0
+// submitas.addEventListener("click", () => {
+//   event.preventDefault();
+//   counter++;
 
-submitas.addEventListener("click", () => {
-    event.preventDefault()
-    counter++
+//   const inputValue = inputas.value;
+//   console.log(inputValue);
+//   const person = {
+//     id: counter,
+//     name: inputValue,
+//   };
+//   arrej.push(person);
+//   console.log(arrej);
+//   localStorage.setItem("zmones", JSON.stringify(arrej));
+// });
+// submitas.addEventListener("click", (a) => {
+//   a.preventDefault();
+//   console.log(a.target);
+// });
 
-    const inputValue = inputas.value
-    console.log(inputValue)
-    const person = {
-        id: counter,
-        name: inputValue
-    }
-arrej.push(person)
-console.log(arrej)
-localStorage.setItem("zmones", JSON.stringify(arrej))
-})
-submitas.addEventListener("click", (a)=>{
-    a.preventDefault()
-    console.log(a.target)
-})
+// const editInput = document.querySelector("#input");
+// const editas = document.querySelector("#editButton");
+// console.log(editInput, editas);
 
+// editas.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   const editable = parseInt(editInput.value);
+//   console.log(editable);
 
-const editInput = document.querySelector("#input")
-const editas = document.querySelector("#editButton")
-console.log(editInput, editas)
+//   const fromLS = JSON.parse(localStorage.getItem("zmones"));
+//   console.log(fromLS);
+//   const founded = fromLS.find((el) => el.id === editable);
+//   console.log(founded);
 
-editas.addEventListener("click", (e)=>{
-    e.preventDefault()
-    const editable = parseInt(editInput.value)
-    console.log(editable)
+//   inputas.value = fromLS[founded].name;
+// });
 
-const fromLS = JSON.parse(localStorage.getItem("zmones"))
-console.log(fromLS)
-const founded = fromLS.find(el => el.id === editable)
-console.log(founded)
+// // ---------------------------------------------------- MOKYTOJOS POV
 
-inputas.value=fromLS[founded].name
-})
+// const forma = document.querySelector("form");
+// const inputas = document.querySelector(".input");
+// const submitas = document.querySelector("form > button");
+// console.log(forma, inputas, submitas);
+
+// const array = [];
+// let counter = 0;
+
+// submitas.addEventListener("click", (event) => {
+//   event.preventDefault();
+//   counter++;
+
+//   const inputoValue = inputas.value;
+//   console.log(inputoValue);
+
+//   const person = {
+//     id: counter,
+//     name: inputoValue,
+//   };
+
+//   array.push(person);
+//   console.log(array);
+//   localStorage.setItem("zmones", JSON.stringify(array));
+
+//   // inputas.value=""
+//   forma.reset();
+// });
+
+// submitas.addEventListener("click", (a) => {
+//   a.preventDefault();
+//   console.log(a.target);
+// });
+
+// const editInput = document.querySelector("#edit");
+// const editas = document.querySelector("#editbutton");
+// console.log(editInput, editas);
+
+// editas.addEventListener("click", (e) => {
+//   e.preventDefault();
+
+//   const editable = parseInt(editInput.value);
+//   console.log(editable);
+
+//   const fromLS = JSON.parse(localStorage.getItem("zmones"));
+//   console.log(fromLS);
+
+//   const founded = fromLS.findIndex((el) => el.id === editable);
+//   console.log(founded);
+
+//   inputas.value = fromLS[founded].name;
+// });
